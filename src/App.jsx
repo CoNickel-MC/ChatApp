71666bfc0f5a12cd06b3a319b4ea7ff279e2356e
CoNickel-MC <springterror228@gmail.com>
2024-09-14 16:42:31 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Direct from './Direct';
 import Login from './Login';
 import Register from './Register';
@@ -9,13 +9,13 @@ function App() {
   const [user, setUser] = useState("Ad");
 
   return (
-	<BrowserRouter>
+	<HashRouter>
 		<Routes>
 			<Route path="/" element={<Login setUser={setUser} />} />
 			<Route path="/direct" element={<Direct username={user} />} />
 			<Route path="/register" element={<Register/>} />
 		</Routes>
-	</BrowserRouter>
+	</HashRouter>
   );
 }
 
