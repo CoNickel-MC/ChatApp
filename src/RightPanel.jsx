2ac827d02beb808c262f32e449155f57c9fc85ac
CoNickel-MC <springterror228@gmail.com>
2024-09-14 16:37:09 +0530

    Initial Commit

---
+++ src/RightPanel.jsx
@@ -0,0 +1,11 @@
+function RightPanel(){
+    return(
+        <div>
+            <div className = "botRightPanel" >
+                <div className = "topRightPanel" />           
+            </div>
+        </div>
+    );
+}
+
+export default RightPanel;
